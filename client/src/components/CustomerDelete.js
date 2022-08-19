939a5ddd4b98378c97cda36718cdafb22b864ab4
ymj <ymj@techheim.com>
2022-08-19 16:13:30 +0900

    [Add Code] add customer delete module

---
+++ client/src/components/CustomerDelete.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class CustomerDelete extends Component {
+    
+    deleteCustomer(id) {
+        const url = '/api/customers/' + id
+        fetch(url, {
+            method: 'DELETE'
+        })
+        this.props.stateRefresh()
+    }
+
+    render() {
+        return (
+            <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>Delete</button>
+        )
+    }
+}
+export default CustomerDelete
