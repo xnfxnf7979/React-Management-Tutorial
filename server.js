9066a9c4bd3b6bfa9e8aabbfa94d76b5c08a65fa
ymj <ymj@techheim.com>
2022-08-19 16:14:05 +0900

    [Add Code] add delete settings

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const upload = multer({dest: './upload'})
 
 app.get('/api/customers', (req,res) => {
     connection.query(
-        "SELECT * FROM CUSTOMER",
+        "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
         (err, rows, fields) => {
             res.send(rows)
         }
@@ -33,7 +33,7 @@ app.get('/api/customers', (req,res) => {
 })
 app.use('/image', express.static('./upload'))
 app.post('/api/customers', upload.single('image'), (req, res) => {
-    let sql = 'INSERT INTO CUSTOMER VALUES (NULL, ?, ?, ?, ?, ?)'
+    let sql = 'INSERT INTO CUSTOMER VALUES (NULL, ?, ?, ?, ?, ?, now(), 0)'
     let image = '/image/' + req.file.filename
     console.log(image)
     let name = req.body.name
@@ -48,6 +48,14 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
             // console.log(rows)
         })
 })  
+app.delete('/api/customers/:id', (req,res)=> {
+    let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?'
+    let params = [req.params.id]
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows)
+        })
+})
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
 
