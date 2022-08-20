a3a73919a9a97ea6e0cbba951fc3a88e6f9c9395
ymj <ymj@techheim.com>
2022-08-20 15:03:06 +0900

    check console log for error

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
     connection.query(sql, params,
         (err, rows, fields)=> {
             res.send(rows)
-            // console.log(err)
+            console.log(err)
             // console.log(rows)
         })
 })  
