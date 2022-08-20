50865b536cd737664b86cde1f26de50ba156887d
ymj <ymj@techheim.com>
2022-08-20 15:01:55 +0900

    [Add Code] add material ui design for delete module

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { Dialog } from "@material-ui/core";
+import {DialogActions} from "@material-ui/core";
+import {DialogTitle} from "@material-ui/core";
+import {DialogContent} from "@material-ui/core";
+import {Button} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 
 class CustomerDelete extends Component {
     
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        })
+    }
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
+
     deleteCustomer(id) {
         const url = '/api/customers/' + id
         fetch(url, {
@@ -12,8 +36,25 @@ class CustomerDelete extends Component {
 
     render() {
         return (
-            <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>Delete</button>
+            <div>
+            <Button variant='contained' color='secondary' onClick={this.handleClickOpen}>Delete</Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle onClose={this.handleClose}>
+                    삭제 경고
+                </DialogTitle>
+                <DialogContent>
+                    <Typography gutterBottom>
+                        선택한 고객 정보가 삭제됩니다.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="primary" onClick={(e)=>{this.deleteCustomer(this.props.id)}}>삭제</Button>
+                    <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                </DialogActions>
+            </Dialog>
+            </div>
         )
     }
 }
 export default CustomerDelete
+
